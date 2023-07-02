f65e550d29610ed494ce26e24b3ee0caa313e8ab
Ajinkya Bhuruk <ajinkyabhuruk.dev@gmail.com>
2023-07-02 23:30:48 +0530

    Refactor outer drawer component

--- src/components/AdminPortal/AdminPortal/AdminPortalHomePage.js
+++ src/components/AdminPortal/AdminPortal/AdminPortalHomePage.js
@@ -2,30 +2,17 @@ import * as React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Home from '@mui/icons-material/Home';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { useNavigate } from 'react-router-dom';
-import { isAuthorizedToPerform } from '../../../utils.js';
-import OuterDrawer from './OuterDrawer.js'
+import OuterDrawer from './LeftMenuPanel/DrawerComponent.js'
 
 const drawerWidth = 240;
-
 
 const Main = styled('main', {
     shouldForwardProp: (props) => props !== 'open',
@@ -45,26 +32,6 @@ const Main = styled('main', {
         marginLeft: 0,
     }),
 }));
-
-// const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-//   open?: boolean;
-// }>(({ theme, open }) => ({
-//   flexGrow: 1,
-//   padding: theme.spacing(3),
-//   transition: theme.transitions.create('margin', {
-//     easing: theme.transitions.easing.sharp,
-//     duration: theme.transitions.duration.leavingScreen,
-//   }),
-//   marginLeft: `-${drawerWidth}px`,
-//   ...(open && {
-//     transition: theme.transitions.create('margin', {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//     marginLeft: 0,
-//   }),
-// }));
-
 
 const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (props) => props !== 'open',
@@ -96,9 +63,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 
 // ['All mail', 'Trash', 'Spam'].
 
-
 const AdminPortalHomePage = () => {
-    const theme = useTheme();
     const [open, setOpen] = React.useState(false);
     const navigate = useNavigate();
 
@@ -113,23 +78,6 @@ const AdminPortalHomePage = () => {
     const navigateToHome = () => {
         navigate('/')
     };
-
-    const canAddNewUser = isAuthorizedToPerform("user", "canAddNewUser");
-
-    const menuOptions2 = [
-        // { menuLabel: 'Another Demo', routePath: 'demomenu2' }
-        // { menuLabel: 'Starred', routePath: 'starred' },
-        // { menuLabel: 'Send email', routePath: 'sendemail'},
-        // { menuLabel: 'Drafts', routePath: 'drafts'}
-    ]
-
-    const menuOptions1 = [
-        { menuLabel: 'Add User', routePath: 'adduser', disabled: !canAddNewUser }
-        // { menuLabel: 'Starred', routePath: 'starred' },
-        // { menuLabel: 'Send email', routePath: 'sendemail'},
-        // { menuLabel: 'Drafts', routePath: 'drafts'}
-    ]
-
 
     return (
         <Box sx={{ display: 'flex' }}>
---
+++ src/components/AdminPortal/AdminPortal/LeftMenuPanel/DrawerComponent.js
@@ -0,0 +1,188 @@
+import * as React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Home from '@mui/icons-material/Home';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+import { isAuthorizedToPerform } from '../../../../utils.js';
+
+const drawerWidth = 240;
+
+// const useStyles = makeStyles({
+//     list: {
+//       width: 250
+//     },
+//     fullList: {
+//       width: "auto"
+//     }
+//   });
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
+
+const DrawerComponent = (props) => {
+    const theme = useTheme();
+
+    const { open, handleDrawerClose } = props;
+    const canAddNewUser = isAuthorizedToPerform("user", "canAddNewUser");
+
+    const menuOptions2 = [
+        // { menuLabel: 'Another Demo', routePath: 'demomenu2' }
+        // { menuLabel: 'Starred', routePath: 'starred' },
+        // { menuLabel: 'Send email', routePath: 'sendemail'},
+        // { menuLabel: 'Drafts', routePath: 'drafts'}
+    ]
+
+    const menuOptions1 = [
+        { menuLabel: 'Add User', routePath: 'adduser', disabled: !canAddNewUser }
+        // { menuLabel: 'Starred', routePath: 'starred' },
+        // { menuLabel: 'Send email', routePath: 'sendemail'},
+        // { menuLabel: 'Drafts', routePath: 'drafts'}
+    ]
+
+    const [menuName, setMenuName] = React.useState(null);
+
+    const mainMenuListArr = [
+        {
+            menuLabel: 'User Section',
+            menuKey: 'usersectionmenukey',
+            noLink: true,
+        },
+        {
+            menuLabel: 'Product Category',
+            menuKey: 'productcategorymenukey',
+            noLink: true,
+        }
+    ];
+
+    const subMenuListArr = {
+        usersectionmenukey: [
+            {
+                menuLabel: 'Add User',
+                menuKey: 'addusermenukey',
+                routePath: 'adduser',
+                disabled: !canAddNewUser
+            },
+            {
+                menuLabel: 'User Search',
+                menuKey: 'usersearchmenukey'
+            }
+        ],
+        productcategorymenukey: [
+            {
+                menuLabel: 'Add Category',
+                menuKey: 'addcategorymenukey',
+            },
+            {
+                menuLabel: 'Category Search',
+                menuKey: 'categorysearchmenukey'
+            }
+        ],
+    };
+
+    const innerList = anchor => {
+        let arr = menuName ? subMenuListArr[menuName] : mainMenuListArr;
+        const clickListener = text => {
+            if (!menuName) {
+                return setMenuName(text);
+            } else {
+                return alert(`${text} clicked`);
+            }
+        };
+        return (
+            <div
+            >
+                <List>
+                    {arr.map((mnitem, index) => {
+
+                        if (mnitem.noLink) {
+                            return (
+                                <ListItem button key={mnitem.menuKey} onClick={() => clickListener(mnitem.menuKey)}>
+                                    <ListItemText primary={mnitem.menuLabel} />
+                                    {!menuName && <ChevronRightIcon />}
+                                </ListItem>
+                            )
+                        }
+
+                        return (
+                            <ListItem key={mnitem.menuKey} disablePadding className={mnitem.disabled ? 'disabled-link-li' : ''}>
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                    </ListItemIcon>
+                                    <Link className={mnitem.disabled ? 'disabled-link' : ''} to={mnitem.routePath}>{mnitem.menuLabel}</Link>
+                                    {/* <ListItemText primary={item.menuLabel} /> */}
+                                </ListItemButton>
+                            </ListItem>
+                        )
+                    }
+                    )}
+                </List>
+            </div>
+        );
+    };
+
+    return (
+        <Drawer
+            sx={{
+                width: drawerWidth,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: drawerWidth,
+                    boxSizing: 'border-box',
+                },
+            }}
+            variant="persistent"
+            anchor="left"
+            open={open}
+        >
+            <DrawerHeader>
+                <IconButton onClick={handleDrawerClose}>
+                    {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+            </DrawerHeader>
+            <Divider />
+            {menuName && (
+                <ListItem button onClick={() => setMenuName(null)}>
+                    <ListItemText primary="Back" />
+                    <ChevronLeftIcon />
+                </ListItem>
+            )}
+            {innerList()}
+            <List>
+                {menuOptions2.map((item, index) => (
+                    <ListItem key={item.menuLabel} disablePadding className={item.disabled ? 'disabled-link-li' : ''}>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <Link className={item.disabled ? 'disabled-link' : ''} to={item.routePath}>{item.menuLabel}</Link>
+                            {/* <ListItemText primary={item.menuLabel} /> */}
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Drawer>
+    );
+}
+
+export default DrawerComponent;
